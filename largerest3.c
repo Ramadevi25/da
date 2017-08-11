ef4ced20b9e80e65aaf0d4bc957b22a524753339
Ramadevi25 <30866169+Ramadevi25@users.noreply.github.com>
2017-08-11 18:19:40 +0530

    Create largerest3.c

---
+++ largerest3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,c;
+	printf("enter three numbers\n");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b)
+	{
+		if(a>c)
+			printf("%d is greater",a);
+		else
+			printf("%d is greater",c);
+	}
+	else if(b>c)
+	{
+		printf("%d is greater",b);
+	}
+    else
+    	printf("%d is greater",c);
+	return 0;
+}
